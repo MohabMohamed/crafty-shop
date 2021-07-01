d1c232db6352d0c7541e840c2921075e764569cd
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2021-07-01 22:01:40 +0200

    Add RefreshToken model

---
+++ src/models/refresh-token.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db/db')
+
+const RefreshToken = sequelize.define(
+  'RefreshToken',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    tableName: 'RefreshToken'
+  }
+)
+
+RefreshToken.associate = models => {
+  RefreshToken.belongsTo(models.User, {
+    foreignKey: 'userId',
+    as: 'refreshToken',
+    onUpdate: 'cascade',
+    onDelete: 'cascade'
+  })
+}
+
+module.exports = RefreshToken
