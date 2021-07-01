ece96d842b0b9b79050134246a19954adab1212e
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2021-07-01 22:26:53 +0200

    Add Order model

---
+++ src/models/order.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db/db')
+
+const Order = sequelize.define(
+  'Order',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    grandTotal: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      defaultValue: 0.0
+    }
+  },
+  {
+    tableName: 'Order'
+  }
+)
+
+Order.associate = models => {
+  Order.belongsTo(models.User, {
+    foreignKey: 'userId',
+    as: 'order',
+    onUpdate: 'cascade',
+    onDelete: 'cascade'
+  })
+}
+
+module.exports = Order
