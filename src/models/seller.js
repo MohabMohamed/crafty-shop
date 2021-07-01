b088780099503823983387c6abfdc4f8003fc3df
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2021-07-01 22:06:19 +0200

    Add Seller model

---
+++ src/models/seller.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db/db')
+
+const Seller = sequelize.define(
+  'Seller',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    tableName: 'Seller',
+    updatedAt: false
+  }
+)
+
+Seller.associate = models => {
+  Seller.belongsTo(models.User, {
+    foreignKey: 'userId',
+    as: 'seller',
+    onUpdate: 'cascade',
+    onDelete: 'cascade'
+  })
+}
+
+module.exports = Seller
