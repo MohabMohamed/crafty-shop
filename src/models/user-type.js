5627487a0103b673a7469323baad37694b9f519d
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2021-07-01 21:42:32 +0200

    Add UserType model

---
+++ src/models/user-type.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db/db')
+
+const UserType = sequelize.define(
+  'UserType',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    type: {
+      type: DataTypes.STRING(20),
+      allowNull: false
+    }
+  },
+  {
+    tableName: 'UserType'
+  }
+)
+
+UserType.associate = models => {
+  UserType.hasMany(models.User, {
+    foreignKey: 'userType',
+    as: 'userType',
+    onUpdate: 'cascade',
+    onDelete: 'SET NULL'
+  })
+}
