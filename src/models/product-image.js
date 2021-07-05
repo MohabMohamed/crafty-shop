22a811902e45f2f2949e2589d2d01506cdd48ab7
Mohab Abd El-Dayem <mohab.m.mohamed@gmail.com>
2021-07-06 00:50:38 +0200

    Add ProductImage model

---
+++ src/models/product-image.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../db/db')
+
+const ProductImage = sequelize.define(
+  'ProductImage',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    updatedAt: false,
+    tableName: 'ProductImage'
+  }
+)
+
+ProductImage.associate = models => {
+  ProductImage.belongsTo(models.Product, {
+    foreignKey: 'productId',
+    as: 'productImage',
+    onUpdate: 'cascade',
+    onDelete: 'cascade'
+  })
+}
+
+module.exports = ProductImage
